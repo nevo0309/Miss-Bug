1fffb7b96ac2305e6f2aacc46dc873fb5670198c
nevo0309 <nevo19970309@gmail.com.com>
2025-04-09 21:07:45 +0300

    Added services of user auth to login and sigup to the app

---
+++ public/services/user.service.js
@@ -0,0 +1,27 @@
+const BASE_URL = '/api/user/'
+
+export const userService = {
+  query,
+  getById,
+  getEmptyCredentials,
+  remove,
+}
+
+function query() {
+  return axios.get(BASE_URL).then(res => res.data)
+}
+
+function getById(userId) {
+  return axios.get(BASE_URL + userId).then(res => res.data)
+}
+function remove(userId) {
+  return axios.delete(BASE_URL + userId).then(res => res.data)
+}
+
+function getEmptyCredentials() {
+  return {
+    username: '',
+    password: '',
+    fullname: '',
+  }
+}
