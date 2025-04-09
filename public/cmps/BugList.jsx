e9a57b8bf472b448bf108979e1a270ac7a9fb906
nevo0309 <nevo19970309@gmail.com.com>
2025-04-09 21:11:21 +0300

    Add the option to edit or remove only to the user logged in

--- public/cmps/BugList.jsx
+++ public/cmps/BugList.jsx
@@ -1,20 +1,36 @@
 const { Link } = ReactRouterDOM
 
 import { BugPreview } from './BugPreview.jsx'
+import { authService } from '../services/auth.service.js'
 
 export function BugList({ bugs, onRemoveBug, onEditBug }) {
+  const user = authService.getLoggedinUser()
 
-    if (!bugs) return <div>Loading...</div>
-    return <ul className="bug-list">
-        {bugs.map(bug => (
-            <li key={bug._id}>
-                <BugPreview bug={bug} />
-                <section className="actions">
-                    <button><Link to={`/bug/${bug._id}`}>Details</Link></button>
-                    <button onClick={() => onEditBug(bug)}>Edit</button>
-                    <button onClick={() => onRemoveBug(bug._id)}>x</button>
-                </section>
-            </li>
-        ))}
-    </ul >
+  function isAllowed(bug) {
+    if (!user) return false
+    if (user.isAdmin || (bug.creator && user._id === bug.creator._id)) return true
+    return false
+  }
+
+  if (!bugs) return <div>Loading...</div>
+  return (
+    <ul className="bug-list">
+      {bugs.map(bug => (
+        <li key={bug._id}>
+          <BugPreview bug={bug} />
+          <section className="actions">
+            <button>
+              <Link to={`/bug/${bug._id}`}>Details</Link>
+            </button>
+            {isAllowed(bug) && (
+              <div>
+                <button onClick={() => onEditBug(bug)}>Edit</button>
+                <button onClick={() => onRemoveBug(bug._id)}>x</button>
+              </div>
+            )}
+          </section>
+        </li>
+      ))}
+    </ul>
+  )
 }
--- public/cmps/BugPreview.jsx
+++ public/cmps/BugPreview.jsx
@@ -1,6 +1,15 @@
-export function BugPreview({bug}) {
-    return <article className="bug-preview">
-        <p className="title">{bug.title}</p>
-        <p>Severity: <span>{bug.severity}</span></p>
+export function BugPreview({ bug }) {
+  return (
+    <article className="bug-preview">
+      <p className="title">{bug.title}</p>
+      <p>
+        Severity: <span>{bug.severity}</span>
+      </p>
+      {bug.creator && (
+        <p>
+          Creator: <span>{bug.creator.fullname}</span>
+        </p>
+      )}
     </article>
+  )
 }
