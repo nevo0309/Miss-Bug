cf0c0f9a2ccaa5cdbaf1e7d028a2e6540cc1280b
nevo0309 <nevo19970309@gmail.com.com>
2025-04-09 21:08:56 +0300

    Add user detail page

--- public/RootCmp.jsx
+++ public/RootCmp.jsx
@@ -1,27 +1,38 @@
 // const Router = ReactRouterDOM.HashRouter
+const { useState } = React
 const Router = ReactRouterDOM.BrowserRouter
 const { Route, Routes } = ReactRouterDOM
+
+import { authService } from './services/auth.service.js'
 
 import { UserMsg } from './cmps/UserMsg.jsx'
 import { AppHeader } from './cmps/AppHeader.jsx'
 import { AppFooter } from './cmps/AppFooter.jsx'
 import { Home } from './pages/Home.jsx'
+import { UserDetails } from './pages/UserDetails.jsx'
 import { BugIndex } from './pages/BugIndex.jsx'
 import { BugDetails } from './pages/BugDetails.jsx'
 import { AboutUs } from './pages/AboutUs.jsx'
+import { LoginSignup } from './pages/LoginSignup.jsx'
+import { UserIndex } from './pages/UserIndex.jsx'
 
 export function App() {
+  const [loggedinUser, setLoggedinUser] = useState(authService.getLoggedinUser())
   return (
     <Router>
       <div className="app-wrapper">
         <UserMsg />
-        <AppHeader />
+        <AppHeader loggedinUser={loggedinUser} setLoggedinUser={setLoggedinUser} />
         <main className="container">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/bug" element={<BugIndex />} />
             <Route path="/bug/:bugId" element={<BugDetails />} />
+            <Route path="/user" element={<UserDetails />} />
+            <Route path="/admin" element={<UserIndex />} />
+
             <Route path="/about" element={<AboutUs />} />
+            <Route path="/auth" element={<LoginSignup setLoggedinUser={setLoggedinUser} />} />
           </Routes>
         </main>
         <AppFooter />
--- public/cmps/AppHeader.jsx
+++ public/cmps/AppHeader.jsx
@@ -1,12 +1,45 @@
-const { NavLink } = ReactRouterDOM
+const { Link, NavLink } = ReactRouterDOM
+const { useNavigate } = ReactRouter
 
-export function AppHeader() {
-    return <header className="app-header main-content single-row">
-        <h1>Miss Bug</h1>
-        <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/bug">Bugs</NavLink>
-            <NavLink to="/about">About</NavLink>
-        </nav>
+import { authService } from '../services/auth.service.js'
+import { showErrorMsg } from '../services/event-bus.service.js'
+import { UserMsg } from './UserMsg.jsx'
+
+export function AppHeader({ loggedinUser, setLoggedinUser }) {
+  const navigate = useNavigate()
+
+  function onLogout() {
+    authService
+      .logout()
+      .then(() => {
+        setLoggedinUser(null)
+        navigate('/auth')
+      })
+      .catch(err => {
+        console.log(err)
+        showErrorMsg(`Couldn't logout`)
+      })
+  }
+
+  return (
+    <header className="app-header full main-layout">
+      <h1>Bug App</h1>
+      <nav className="app-nav">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/bug">Bugs</NavLink>
+        {loggedinUser && loggedinUser.isAdmin && <NavLink to="/admin">Admin</NavLink>}
+
+        {!loggedinUser ? (
+          <NavLink to="/auth">Login</NavLink>
+        ) : (
+          <div className="user">
+            <Link to={'/user'}>{loggedinUser.fullname}</Link>
+            <button onClick={onLogout}>logout</button>
+          </div>
+        )}
+      </nav>
+      <UserMsg />
     </header>
+  )
 }
